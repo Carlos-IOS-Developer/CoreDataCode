035169e9be203882093226f483b71c71e899f07e
Carlos <carlosCarlos@carloss-macbook-pro.local>
2018-01-15 00:44:23 +0200

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Carlos on 1/15/18.
+//  Copyright © 2018 Carlos-iv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
